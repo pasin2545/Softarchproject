f6b1f95e9768f70b59fa8c88a83dae28140f3a03
-Nammyo- <69385808+pasin2545@users.noreply.github.com>
2022-11-14 17:26:57 +0700

    Add files via upload

--- KMITLnews_front/softapp/lib/pages/userdisplay.dart
+++ KMITLnews_front/softapp/lib/pages/userdisplay.dart
@@ -1,46 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:softapp/widgets/circle_button.dart';
+import 'package:softapp/widgets/create_post_contrainer.dart';
+import 'package:softapp/widgets/navigation_drawer.dart';
+import 'package:softapp/widgets/post_contrainer.dart';
 
-class userdisplay extends StatelessWidget {
+class userdisplay extends StatefulWidget {
   const userdisplay({Key? key}) : super(key: key);
+
+  @override
+  State<userdisplay> createState() => _userdisplayState();
+}
+
+class _userdisplayState extends State<userdisplay> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  void _closeEndDrawer() {
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'KMITL',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Row(
+      key: _scaffoldKey,
+      endDrawer: NavigateDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Color.fromARGB(255, 222, 105, 21),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(
-                  'NEWS',
+                  'KMITL',
                   style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white,
-                  ),
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'NEWS',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-        backgroundColor: Color.fromARGB(255, 222, 105, 21),
-        actions: <Widget>[
-            IconButton(
-              onPressed: () => print("กดกระดิ่ง"), 
-              icon: FaIcon(FontAwesomeIcons.bell),
+            actions: <Widget>[
+              CircleButton(
+                  icon: FaIcon(FontAwesomeIcons.bell),
+                  iconSize: 23.0,
+                  onPressed: () => print('bell')),
+              CircleButton(
+                  icon: FaIcon(FontAwesomeIcons.gripVertical),
+                  iconSize: 23.0,
+                  onPressed: _openEndDrawer,),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              child: CreatePostContrainer(
+                image: NetworkImage('shorturl.at/cmtQ4'),
               ),
-            IconButton(
-              onPressed: () => print("กดสามปุ่ม"), 
-              icon: FaIcon(FontAwesomeIcons.gripVertical),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Expanded(
+              child: Container(
+                child: PostContainer(),
               ),
+            ),
+          ),
         ],
       ),
     );
